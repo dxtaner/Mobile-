1245c6dce90ce3bd1e2d0bfcbc35ceac249258f4
Taner Özer 😌 <tanerozer16@gmail.com>
2025-04-03 08:53:09 +0300

    ShoopingApp

---
+++ MyShoppingApp/config.js
@@ -0,0 +1,4 @@
+// config.js
+export const API_URL = 'https://fakestoreapi.com/auth/login';
+export const API_URL_PRODUCTS = 'https://fakestoreapi.com/products';
+export const API_URL_PRODUCT_DETAIL = 'https://fakestoreapi.com/products/';
