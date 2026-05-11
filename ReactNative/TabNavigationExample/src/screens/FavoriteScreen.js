17327b828a103e248568c79217bbbc5dca75eebb
Taner Özer <tanerozer16@gmail.com>
2026-05-11 14:19:01 +0300

    Create FavoriteScreen.js

---
+++ ReactNative/TabNavigationExample/src/screens/FavoriteScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FavoriteScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>❤️ Favorite Screen</Text>
+    </View>
+  );
+};
+
+export default FavoriteScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    color: '#fff',
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+});
