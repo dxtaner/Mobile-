114233e104cdaa27047733f49f460d657125e5d4
Taner Özer <tanerozer16@gmail.com>
2026-05-15 14:36:49 +0300

    Create songs.js

---
+++ ReactNative/MusicPlayerApp/src/data/songs.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    id: '1',
+    title: 'Night Drive',
+    artist: 'Synth Wave',
+    image: 'https://picsum.photos/400?1',
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+  },
+
+  {
+    id: '2',
+    title: 'Dreamscape',
+    artist: 'Chill Beats',
+    image: 'https://picsum.photos/400?2',
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+  },
+
+  {
+    id: '3',
+    title: 'Cyber City',
+    artist: 'Neon Music',
+    image: 'https://picsum.photos/400?3',
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3',
+  },
+];
